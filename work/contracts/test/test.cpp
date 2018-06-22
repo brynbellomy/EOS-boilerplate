04d0283872af8fede49861febfb394fa71010191
bryn austin bellomy <bryn.bellomy@gmail.com>
2018-06-22 15:58:02 -0500

    Add storing and fetching data

--- work/contracts/test/test.cpp
+++ work/contracts/test/test.cpp
@@ -2,14 +2,35 @@
 
 using namespace eosio;
 
-class pingpong : public eosio::contract {
+class pingpong : public eosio::contract
+{
   public:
-      using contract::contract;
+    using contract::contract;
 
-      /// @abi action
-      void ping( account_name user ) {
+    /// @abi action
+    void ping( account_name user ) {
          print( "Pong, ", name{user} );
-      }
+
+         require_auth(user);
+
+         playerIndex players(_self, _self);
+         auto iterator = players.find(user);
+         players.emplace(user, [&](auto& player) {
+            player.username = user;
+            player.level = 123;
+        });
+    }
+
+    //@abi table player i64
+    struct player {
+        account_name username;
+        uint64_t level;
+
+        uint64_t primary_key() const { return username; }
+
+        EOSLIB_SERIALIZE(player, (username)(level))
+    };
+    typedef multi_index<N(player), player> playerIndex;
 };
 
 EOSIO_ABI( pingpong, (ping) )
--- work/web/index.js
+++ work/web/index.js
@@ -1,20 +1,40 @@
 const EOS = require('eosjs')
 const keystore = require('../keystore.json')
 
+var eos
+
+main()
+
 async function main() {
+    await initEOSJS()
+    await doTransaction()
+    await fetchResult()
+}
+
+async function initEOSJS() {
     const chainId = (await EOS().getInfo({})).chain_id
-    const eos = EOS({
+    eos = EOS({
         keyProvider: [ keystore.accounts['test.code'].activePrivateKey ],
         chainId,
     })
 
-    eos.getCurrencyBalance('eosio.token', 'eosio').then(x => console.log(x))
+    // eos.getCurrencyBalance('eosio.token', 'eosio').then(x => console.log(x))
     // eos.getCurrencyStats()
-
-    const pingContract = await eos.contract('test.code')
-    const receipt = await pingContract.ping('blah', {authorization: 'test.code@active'})
-    console.log('Receipt:', receipt)
-
 }
 
-main()
+async function doTransaction() {
+    const pingContract = await eos.contract('test.code')
+    const receipt = await pingContract.ping('test.code', {authorization: 'test.code@active'})
+    console.log('Receipt:', receipt)
+}
+
+async function fetchResult() {
+    let resp = await eos.getTableRows({
+        json: true,
+        code: 'test.code',
+        table: 'player',
+        scope: 'test.code', // this is the index to fetch
+    })
+
+    console.log('fetched ~>', resp)
+}
